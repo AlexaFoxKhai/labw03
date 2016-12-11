0188223ed6b6507110184465bafe3e9288d4e31f
AlexaFoxKhai <alexababiy118@gmail.com>
2016-12-11 14:26:12 +0200

    Create proverka.java

---
+++ proverka.java
@@ -0,0 +1,9 @@
+public class proverka {
+	public static void main(String[] args) {
+		labw03 l = new labw03();
+		System.out.println(l.logika(30,40,50));
+		l.poka(4);
+		double mas[] = {3.1, 2.1, 0, 1.5, 95.13};
+		l.massivy(mas,2,3);
+	}
+}
